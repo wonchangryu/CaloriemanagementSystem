c4f0803613c8057a5482e3c60730dde15444722b
wonchangryu <sktinovation@gmail.com>
2021-03-10 12:19:00 +0900

    added Calorie.java

---
+++ src/Calorie.java
@@ -0,0 +1,12 @@
+
+public class Calorie {
+
+	public static void main(String[] args) {
+		System.out.print("1.Add users");
+		System.out.print("2.Delete users");
+		System.out.print("3.Edit users");
+		System.out.print("4.View users");
+		System.out.print("5.Exit");
+	}
+
+}
